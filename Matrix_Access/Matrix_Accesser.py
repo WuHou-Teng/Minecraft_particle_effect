ebbed4eae006354f908b14cb937445f9e8a9915f
WuHou_Teng <toniteng2@gmail.com>
2023-01-09 17:36:58 +1000

    为MatrixAccesser添加了功能：默认文件夹搜索，以及矩阵信息收集。 添加了默认文件夹目录。

--- Matrix_Access/Matrix_Accesser.py
+++ Matrix_Access/Matrix_Accesser.py
@@ -1,4 +1,7 @@
 import copy
+import os
+from Matrix_Access.Matrix_Const import *
+import numpy as np
 
 
 class MatrixAccesser(object):
@@ -8,21 +11,64 @@ class MatrixAccesser(object):
     构造函数会在创建时尝试遍历整个粒子矩阵文件，并将相应的参数加载，备用。
     """
     def __init__(self, matrix_file_name):
+        self.cwd = os.getcwd()
         # 粒子矩阵文件名称
         self.mat_file = matrix_file_name
+        # 载入默认路径
+        self.default_path = []
+        self.load_matrix_file_path()
         # 直接读取粒子矩阵文件
-        self.mat_array = self.read_mat()
+        self.mat_list = self.read_mat()
+
+        # 最大粒子数量
+        self.particle_num = 0
+        # 个方向上的方位列表
+        self.x_array = None
+        self.y_array = None
+        self.z_array = None
+        # 各方向上的极值
+        self.x_max = 0
+        self.x_min = 0
+        self.y_max = 0
+        self.y_min = 0
+        self.z_max = 0
+        self.z_min = 0
+        # 各方向极值坐标组成的长方体的中心点
+        self.geom_centre = [0, 0, 0]
+        # 个方向上坐标的平均值组成的中心点
+        self.mean_centre = [0, 0, 0]
+        # 延时
+        self.delay_type = ADDITIONAL
+        self.max_delay = 0
+        self.delay_array = None
+
+        # self.get_extra_info() 默认不调用，因为可能比较耗时间。
+
+    def load_matrix_file_path(self):
+        with open("./default_matrix_address.txt", "r", encoding="UTF-8") as dm_address:
+            new_addresses = dm_address.readlines()
+            for lines in new_addresses:
+                if not lines.startswith("#"):
+                    self.default_path.append(lines.strip())
 
     def get_name(self):
         return self.mat_file
 
     def get_mat_array(self):
         # 注意，返回内容一定是deep copy
-        return copy.deepcopy(self.mat_array)
+        return copy.deepcopy(self.mat_list)
 
     def set_mat_file(self, matrix_file_name):
         self.mat_file = matrix_file_name
-        self.mat_array = self.read_mat()
+        self.mat_list = self.read_mat()
+
+    def matrix_file_found(self, mat_file):
+        if os.path.exists(mat_file):
+            return mat_file
+        for path in self.default_path:
+            if os.path.exists(os.path.join(path, mat_file)):
+                return os.path.join(path, mat_file)
+        return None
 
     def read_mat(self):
         """
@@ -33,14 +79,16 @@ class MatrixAccesser(object):
             mat_array: 保存了整个矩阵的列表
         """
         mat_array = []
-        if self.mat_file is not None:
-            with open(self.mat_file, "r") as mat:
+        # 首先检索一边默认的文件夹。
+        search_result = self.matrix_file_found(self.mat_file)
+        if search_result is not None:
+            with open(search_result, "r") as mat:
                 mat_data = mat.readlines()
                 for particles in mat_data:
                     # print(particles)
                     if len(particles) > 0 and particles[0] != "#":
                         particles_info = particles.strip().split(',')
-                        if len(particles_info) > 10:
+                        if len(particles_info) == 17:
                             for i in range(len(particles_info)):
 
                                 particles_info[i] = particles_info[i].strip()
@@ -83,7 +131,7 @@ class MatrixAccesser(object):
 
         return particle
 
-    # TODO 需要考虑计算的参数如下：
+    # 整个矩阵内所有点的数量。
     # 各方向极点坐标
     #   x方向数值最小，最大的点
     #   y方向数值最小，最大的点
@@ -91,8 +139,52 @@ class MatrixAccesser(object):
     # 矩阵外接长方体（该长方体与xyz轴对齐。）
     #   各顶点坐标
     #   长方体中心坐标
-    # 矩阵外接圆
-    #   圆心坐标
-    #   圆的半径
+
     # 矩阵中所有粒子可能的延时时间。做一个列表，并返回。
+
+    def get_extra_info(self):
+        """
+
+        :return:
+        """
+        print("正在提取矩阵信息")
+        self.particle_num = len(self.mat_list)
+
+        mat_array = np.array(self.mat_list)
+        self.x_array = mat_array[:, 0]
+        self.y_array = mat_array[:, 1]
+        self.z_array = mat_array[:, 2]
+        self.delay_array = mat_array[:, 16]
+        # for particle in self.mat_list:
+        #     self.x_list.append(particle[0])
+        #     self.y_list.append(particle[1])
+        #     self.z_list.append(particle[2])
+        #     self.delay_list.append(particle[16])
+
+        # x_array = np.array(self.x_list)
+        # y_array = np.array(self.y_list)
+        # z_array = np.array(self.z_list)
+        # delay_list = np.array(delay_list)
+
+        # 获取最大时间。
+        if self.delay_type is ADDITIONAL:
+            self.max_delay = np.sum(self.delay_array)
+        elif self.delay_type is ABSOLUTE:
+            self.max_delay = np.max(self.delay_array)
+
+        self.x_max = np.max(self.x_array)
+        self.x_min = np.min(self.x_array)
+        self.y_max = np.max(self.y_array)
+        self.y_min = np.min(self.y_array)
+        self.z_max = np.max(self.z_array)
+        self.z_min = np.min(self.z_array)
+
+        self.geom_centre = [(self.x_max + self.x_min)/2,
+                            (self.y_max + self.y_min)/2,
+                            (self.z_max + self.z_min)/2]
+        self.mean_centre = [self.x_array.mean(),
+                            self.y_array.mean(),
+                            self.z_array.mean()]
+
+        print("完成信息提取")
 
